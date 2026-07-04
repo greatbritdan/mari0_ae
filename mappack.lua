db36350e4d19339a3d9dbb98b74c824fd6a12101
Britdan <71894636+greatbritdan@users.noreply.github.com>
2026-07-04 14:09:12 +0100

    Add mappack setting 1/3

--- mappack.lua
+++ mappack.lua
@@ -26,6 +26,8 @@ function loadmappacksettings(suspended)
 			continuesublevelmusic = true
 		elseif s2[1] == "nolowtime" then
 			nolowtime = true
+		elseif s2[1] == "disablewallclipping" then
+			disablewallclipping = true
 		elseif s2[1] == "usesuspends" then
 			alwaysdeletesuspend = true
 		elseif s2[1] == "character" then
